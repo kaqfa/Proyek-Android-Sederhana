dfdfbeecebd69450da6b04fa6af556224debafda
Fahri Firdausillah <elfaatta@gmail.com>
2022-09-22 04:05:15 +0700

    Penambahan button toast

--- app/src/main/java/id/my/fahrifirdaus/proyeksederhana/MainActivity.java
+++ app/src/main/java/id/my/fahrifirdaus/proyeksederhana/MainActivity.java
@@ -3,6 +3,9 @@ package id.my.fahrifirdaus.proyeksederhana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNotif = findViewById(R.id.btn_notif);
+        btnNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Selamat data di Android", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
